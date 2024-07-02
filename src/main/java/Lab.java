0581bba64fbc705776039a2a645999ee8ffe3a0d
Finn Chastly Manapat <chastly_regino@icloud.com>
2024-07-02 19:04:02 +0000

    Array index and creation manipulation

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -10,12 +10,14 @@
  * https://www.learnjavaonline.org/en/Arrays
  */
 public class Lab {
+    private static final int[][] Arr = null;
+
     /**
      * @param arr the array we're getting data from.
      * @return the length of arr.
      */
     public int getLengthOfArray(int[] arr){
-        return 0;
+        return arr.length;
     }
 
 
@@ -27,7 +29,7 @@ public class Lab {
      * @return the element at index n of arr.
      */
     public int getNthElementOfArray(int[] arr, int n){
-        return 0;
+        return arr[n];
     }
 
     /**
@@ -39,7 +41,7 @@ public class Lab {
      * @return nothing. Because of pass-by-reference, any change to the array will be reflected across our java program.
      */
     public void setNthElementOfArray(int[] arr, int n, int val){
-
+        arr[n] = val;
     }
 
     /**
@@ -47,6 +49,6 @@ public class Lab {
      * @return a new int array that can contain n elements.
      */
     public int[] returnNewArraySizeN(int n){
-        return null;
+        return new int[n];
     }
 }
